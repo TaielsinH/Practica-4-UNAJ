63ede86ea7c0477eafaf8b51328ec7aea8b7c77e
TaielsinH <105081145+TaielsinH@users.noreply.github.com>
2022-05-06 19:29:15 -0300

    solución punto 3

---
+++ Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practica_4_UNAJ
+{
+	/// <summary>
+	/// Description of Persona.
+	/// </summary>
+	public class Persona
+	{
+		private string nombre;
+		private int edad;
+		private int DNI;
+		
+		public Persona(string nombre, int edad, int DNI)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+			this.DNI = DNI;
+		}
+		public void imprimirPersona()
+		{
+			Console.WriteLine(nombre +" (" + edad + ") " + DNI);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace Practica_4_UNAJ
 		{
 			Hora h = new Hora(23, 30, 15);
 			Hora h2 = new Hora("13:30:15");
+			Persona p = new Persona("Taiel Ojeda Studer", 19, 33333333);
 			
 			h.imprimir();
 			h2.imprimir();
+			
+			p.imprimirPersona();
 			
 			Console.ReadKey(true);
 		}
