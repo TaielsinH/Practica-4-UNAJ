568d820538734557198e045014ec7692b5f90359
TaielsinH <TaielsinH@gmail.com>
2022-05-06 18:02:12 -0300

    Se agrega el proyecto wachoo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practica_4_UNAJ
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Hora h = new Hora(23, 30, 15);
+			Hora h2 = new Hora("13:30:15");
+			
+			h.imprimir();
+			h2.imprimir();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ hora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practica_4_UNAJ
+{
+	
+	public class Hora
+	{
+		private int hora;
+		private int minuto;
+		private int segundo;
+		public Hora(int hora, int minuto, int segundo)
+		{
+			this.hora = hora;
+			this.minuto = minuto;
+			this.segundo = segundo;
+			
+		}
+		public Hora(string cadena)
+		{
+			char separador = ':';
+			string [] partes = cadena.Split(separador);
+			hora = int.Parse(partes[0]);
+			minuto = int.Parse(partes[1]);
+			segundo = int.Parse(partes[2]);
+			
+			
+		}
+		
+		public void imprimir()
+		{
+			Console.WriteLine(hora + " HORAS, " + minuto + " MINUTOS Y " + segundo +" SEGUNDOS");
+		}
+	}
+}
