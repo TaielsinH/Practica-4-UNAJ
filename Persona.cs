2ded3c82bdee6595ee7def65bbe0d2429381a79c
TaielsinH <105081145+TaielsinH@users.noreply.github.com>
2022-05-06 20:43:48 -0300

    Error con el metodo .Today en DateTime

--- Persona.cs
+++ Persona.cs
@@ -10,6 +10,7 @@ namespace Practica_4_UNAJ
 		private string nombre;
 		private int edad;
 		private int DNI;
+		private DateTime FechaNacimiento;
 		
 		public Persona(string nombre, int edad, int DNI)
 		{
@@ -20,6 +21,25 @@ namespace Practica_4_UNAJ
 		public void imprimirPersona()
 		{
 			Console.WriteLine(nombre +" (" + edad + ") " + DNI);
+		}
+		public Persona(string cadena)
+		{
+			char separador = '	';
+			string[] partes = cadena.Split(separador);
+			this.nombre = partes[0];
+			this.DNI = int.Parse(partes[1]);
+			this.edad = int.Parse(partes[2]);	                      
+		}
+		public Persona(string nombre, int DNI, DateTime FechaNacimiento)
+		{
+			
+			DateTime hoy = DateTime.Today(); //Error: Non-invocable member 'System.DateTime.Today' cannot be used like a method
+			this.nombre = nombre;
+			this.DNI = DNI;
+			this.FechaNacimiento = FechaNacimiento;
+			
+			TimeSpan diferencia = hoy - FechaNacimiento;
+			int dias = (int) Math.Floor(diferencia.TotalDays);
 		}
 	}
 }
